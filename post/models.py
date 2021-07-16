f0b202ca67bd33bdb5430ef169d6d9f2d5761c66
Your Name <you@example.com>
2021-07-16 17:33:14 +0500

    first commit

---
+++ post/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    title=models.CharField('post title', max_length=50)
+    body=models.TextField(' Text body')
+
+
+    def __str__(self):
+        return self.title
+    
